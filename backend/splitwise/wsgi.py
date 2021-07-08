3417fe0155de31c0d74200fc4fb81b0a00a2c906
vahidzee <vahid98zee@gmail.com>
2021-07-08 19:51:20 +0430

    add environmental variable management to wsgi

--- backend/splitwise/wsgi.py
+++ backend/splitwise/wsgi.py
@@ -10,7 +10,9 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+import dotenv
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'splitwise.settings')
-
+if os.path.isfile('.env'):
+    dotenv.load_dotenv('.env')
 application = get_wsgi_application()
