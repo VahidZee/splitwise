eb7670b50255af6e915e5c0dad09b125b28c7016
vahidzee <vahid98zee@gmail.com>
2021-07-10 16:00:56 +0430

    add login api

--- backend/splitwise/urls.py
+++ backend/splitwise/urls.py
@@ -15,18 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework import routers
-import splitwise.apps.users.views as user_views
-
-router = routers.DefaultRouter()
-router.register(r'account', user_views.UserViewSet)
-# router.register()
-
-# router.register(r'groups', views.GroupViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)),
     path('admin/', admin.site.urls),
-    path(r'auth', user_views.CustomAuthToken.as_view())
-    # path('auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('user/', include(('splitwise.apps.users.urls', 'users_api'), 'users_api'))
 ]
