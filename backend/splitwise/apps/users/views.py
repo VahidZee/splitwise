75220639cda4af5af386a1bd0118806550ceb91f
vahidzee <vahid98zee@gmail.com>
2021-07-14 11:57:31 +0200

    add `user` api documentation/ fix api permissions

--- backend/splitwise/apps/users/views.py
+++ backend/splitwise/apps/users/views.py
@@ -21,17 +21,22 @@ class UserViewSet(
 ):
     """
     API for user information management and retrieval
+
+    * **login** [`/user/login/|POST`]: obtain a valid authentication token by sending valid credentials url
+    * **logout** [`/user/logout/|POST`]: invalidate currently owned authentication token
+    * **retrieve** [`/user/<username>/|GET`]: obtain user information (by looking up username)
+    * **update** [`/user/<ego-username>/|POST`]: update ego user's information (excluding the password)
+    * **change_password** [`/user/<ego-username>/change_password|POST`]: update user password (old password is required)
     """
 
     lookup_field = 'username'
     lookup_url_kwarg = 'username'
-    permission_classes = [IsAuthenticated]
     authentication_classes = [SessionAuthentication, TokenAuthentication]
     serializer_class = serializers.EgoUserSerializer
     queryset = models.User.objects.all()
 
     def get_view_name(self):
-        return getattr(self, 'name', 'User API') or 'User API'
+        return getattr(self, 'name', 'User') or 'User'
 
     def get_serializer_class(self):
         if self.action == 'create':
@@ -54,18 +59,25 @@ class UserViewSet(
         """
         Instantiates and returns the list of permissions that this view requires.
         """
-        if self.action == 'create' or self.action == 'login':
+        if self.action in ['create', 'login']:
             permission_list = [AllowAny]
-        elif self.action == 'update' or self.action == 'change_password':
-            permission_list = [IsOwnerOrAdmin]
+        elif self.action in ['update', 'change_password', 'logout']:
+            permission_list = [IsSelfOrAdmin, IsAuthenticated]
+        elif self.action == 'retrieve':
+            permission_list = [IsAuthenticated]
         else:
-            permission_list = self.permission_classes
+            permission_list = [AllowAny]
         return [permission() for permission in permission_list]
 
     @action(methods=['POST'], detail=False, )
     def change_password(self, request, format=None):
         """
-        Change user's passcode API.
+        Change user's passcode API by providing the old password.
+
+        **Permissions**:
+
+        * Authentication is required
+        * API only available to _Admins_ or the _Owner_ of the account
         """
         user = request.user
         serializer = self.get_serializer(data=request.data)
@@ -91,7 +103,7 @@ class UserViewSet(
     @action(methods=['POST'], detail=False, )
     def login(self, request, format=None):
         """
-        Login API: in response to a valid username and passcode an Authentication Token is sent
+        Obtain an authentication token by providing valid credentials.
         """
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
@@ -102,7 +114,11 @@ class UserViewSet(
     @action(methods=['POST'], detail=False, )
     def logout(self, request, format=None):
         """
-        Logout API: invalidate Authentication token
+         Invalidate the currently owned authentication token.
+
+         **Permissions** :
+
+         * _Authentication_ is required
         """
         shortcuts.get_object_or_404(Token, user=request.user).delete()
         return Response(status=status.HTTP_202_ACCEPTED)
