bf31a0785bf3467a8edf84046ee55de1f9b3dc06
vahidzee <vahid98zee@gmail.com>
2021-07-15 13:28:22 +0200

    add `me` to user api

--- backend/splitwise/apps/users/views/user.py
+++ backend/splitwise/apps/users/views/user.py
@@ -18,8 +18,9 @@ class UserViewSet(
     * **login** [`/user/login/|POST`]: obtain a valid authentication token by sending valid credentials url
     * **logout** [`/user/logout/|POST`]: invalidate currently owned authentication token
     * **retrieve** [`/user/<username>/|GET`]: obtain user information (by looking up username)
-    * **update** [`/user/<ego-username>/|PUT`]: update ego user's information (excluding the password)
-    * **change_password** [`/user/<ego-username>/change_password|POST`]: update user password (old password is required)
+    * **update** [`/user/me/|PUT`]: update ego user's information (excluding the password)
+    * **retrieve** [`/user/me/|GET`]: obtain current user information
+    * **change_password** [`/user/change_password|POST`]: update user password (old password is required)
     """
 
     lookup_field = 'username'
@@ -34,7 +35,7 @@ class UserViewSet(
     def get_serializer_class(self):
         if self.action == 'create':
             return serializers.RegistrationSerializer
-        elif self.action in ['retrieve', 'update']:
+        elif self.action in ['retrieve', 'update', 'me']:
             if self.request.user.is_staff or \
                     self.request.user.username == self.request.parser_context['kwargs']['username']:
                 return serializers.EgoUserSerializer
@@ -59,13 +60,31 @@ class UserViewSet(
         """
         if self.action in ['create', 'login']:
             permission_list = [drf_permissions.AllowAny]
-        elif self.action in ['update', 'change_password', 'logout', 'friends']:
+        elif self.action in ['update', 'change_password', 'logout', 'friends', 'me']:
             permission_list = [permissions.IsSelfOrAdmin, drf_permissions.IsAuthenticated]
         elif self.action in ['retrieve', 'friend']:
             permission_list = [drf_permissions.IsAuthenticated]
         else:
             permission_list = [drf_permissions.AllowAny]
         return [permission() for permission in permission_list]
+
+    @action(methods=['GET', 'PUT'], detail=False)
+    def me(self, request):
+        """
+        Retrieve and change the current user's account information.
+
+        **Permissions**:
+
+        * _Authentication_ is required
+        * API only available to _Owner_ of the account
+        """
+        if request.method == 'GET':
+            self.kwargs['username'] = request.user.username
+            return self.retrieve(request)
+        elif request.method == 'PUT':
+            self.kwargs['username'] = request.user.username
+            return self.update(request)
+        return Response(status=status.HTTP_404_NOT_FOUND)
 
     @action(methods=['POST'], detail=False, )
     def change_password(self, request, format=None):
@@ -74,7 +93,7 @@ class UserViewSet(
 
         **Permissions**:
 
-        * Authentication is required
+        * _Authentication_ is required
         * API only available to _Admins_ or the _Owner_ of the account
         """
         user = request.user
@@ -119,5 +138,5 @@ class UserViewSet(
 
          * _Authentication_ is required
         """
-        shortcuts.get_object_or_404(models.Token, user=request.user).delete()
+        shortcuts.get_object_or_404(Token, user=request.user).delete()
         return Response(status=status.HTTP_202_ACCEPTED)
