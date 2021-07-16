398b6004f158a66348357a8cfb327f691b18298f
vahidzee <vahid98zee@gmail.com>
2021-07-16 14:18:26 +0200

    resetup CORS headers

--- backend/splitwise/settings.py
+++ backend/splitwise/settings.py
@@ -56,13 +56,13 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
+    'corsheaders.middleware.CorsPostCsrfMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    # 3rd party middlewares
-    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'splitwise.urls'
